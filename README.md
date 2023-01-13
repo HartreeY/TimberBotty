be4e216b61c9e4708b359b4af722a2f8f427c4b7
Vindenatt <roentg@gmail.com>
2023-01-14 00:46:30 +0900

    added delay parameter, readme change

--- README.md
+++ README.md
@@ -6,12 +6,12 @@ Auto-playing bot for Timberman for Windows.
 ## How to use
 1. Download **main.exe**.
 2. Launch **Timberman**.
-3. In the Options, have it run in **Windowed** mode at **720x480**.
+3. Make sure it's in **Windowed** mode.
 4. Start "**Singleplayer**".
 5. Click "**Start**" in the bot.
 ## Info
 Tested on the Steam version of Timberman v2.10, but should work on any version. 
 
-Tested on a mediocre machine; perfomance might differ on different machines. The bot can reach 2007 score with the current program.
+Tested on a mediocre machine; perfomance might differ on different machines. The bot can reach ~2030 score with **delay** = 0.13.
 
 Note: sometimes the bot fails to read the screen. In that case simply click "Stop" and "Start" again.
--- main.py
+++ main.py
@@ -17,6 +17,8 @@ pixel_upper1_R = None
 
 proc = None
 
+multi = False
+
 
 def get_pixel(left,top):
     with mss() as sct:
@@ -28,13 +30,13 @@ def start_pixels():
     pixel_upper1_L = get_pixel(272,234) #remember the branch two "tiles" above the player on the left
     pixel_upper1_R = get_pixel(450,234) #remember the branch two "tiles" above the player on the right
         
-def locate_branch(wsh):
+def locate_branch(wsh,delay):
     global pixel_upper1_L, pixel_upper1_R, direction
     if direction:
         current_pixel = get_pixel(272,234) #left branch check
         if pixel_upper1_L!=current_pixel:
             wsh.SendKeys("{LEFT}")
-            time.sleep(DELAY) #might get better score by tweaking this
+            time.sleep(delay) #might get better score by tweaking this
             wsh.SendKeys("{RIGHT}")
             direction = False
         else:
@@ -43,15 +45,15 @@ def locate_branch(wsh):
         current_pixel = get_pixel(450,234) #right branch check
         if pixel_upper1_R!=current_pixel:
             wsh.SendKeys("{RIGHT}")
-            time.sleep(DELAY) #might get better score by tweaking this
+            time.sleep(delay) #might get better score by tweaking this
             wsh.SendKeys("{LEFT}")
             direction = True
         else:
             wsh.SendKeys("{RIGHT}")
 
-    time.sleep(DELAY)
+    time.sleep(delay)
 
-def shell_main():
+def shell_main(delay):
     end_this = False
     try:
         wsh = comclt.Dispatch("WScript.Shell")
@@ -65,7 +67,7 @@ def shell_main():
         end_this = True
     
     while not end_this:
-        locate_branch(wsh)
+        locate_branch(wsh,delay)
     
             
 
@@ -74,24 +76,45 @@ def act_start():
         game_win = FindWindow(None, "Timberman")
         MoveWindow(game_win,0,0,720,480,True)
         global proc
-        proc = Process(target=shell_main)
+        proc = Process(target=shell_main,args=(DELAY,))
         proc.start()
         btn_start.config(command=act_stop,text = "Stop")
         lbl_status.config(text = "Started successfully.")
     else:
         lbl_status.config(text = "Timberman is not launched.")
 
+def on_delay_change(e):
+    global DELAY,multi
+    if multi:
+        DELAY = float(e)*10
+    else:
+        DELAY = float(e)
 
+def set_delay_multi():
+    global DELAY,multi
+    if multi:
+        btn_delay_multi.config(relief=tk.RAISED)
+        multi = False
+        DELAY /= 10
+    else:
+        btn_delay_multi.config(relief=tk.SUNKEN)
+        multi = True
+        DELAY *= 10
         
 def act_stop():
-    if proc:
+    try:
         proc.terminate()
+    except Exception:
+        lbl_status.config(text = "Process termination error.")
     btn_start.config(command=act_start,text = "Start")
     lbl_status.config(text = "Stopped successfully.")
+    proc.join()
 
 def act_exit():
-    if proc:
+    try:
         proc.terminate()
+    except Exception:
+        lbl_status.config(text = "Process termination error.")
     root.destroy()
 
 if __name__ == '__main__':
@@ -100,7 +123,9 @@ if __name__ == '__main__':
 
 
     root = tk.Tk()
-    root.overrideredirect(True)
+    root.title('TimberBotty')
+    #root.overrideredirect(True)
+    root.resizable(0,0)
     root.geometry("+0+480")
 
     frm = ttk.Frame(root, padding=18)
@@ -108,14 +133,21 @@ if __name__ == '__main__':
 
     lbl_title = ttk.Label(frm,text="TimberBotty",font="Helvetica 18 bold")
     lbl_title.grid(row=0,column=0,columnspan=2)
-    lbl_desc = tk.Label(frm,text=" To use the bot:\n・Launch Timberman\n・Have it run in Windowed (720x480)\n・Press \"Singleplayer\", then \"Start\" here",font="Helvetica 14",justify="left")
+    lbl_desc = tk.Label(frm,text=" To use the bot:\n・Launch Timberman\n・Make sure it's in Windowed mode\n・Press \"Singleplayer\", then \"Start\" here",font="Helvetica 14",justify="left")
     lbl_desc.grid(row=1,column=0,columnspan=2,pady=10)
     btn_start = tk.Button(frm, text="Start", command=act_start,font="sans 14 bold")
-    btn_start.grid(row=2,column=0)
+    btn_start.grid(row=3,column=0)
+    lbl_title = ttk.Label(frm,text="Delay:",font="Helvetica 14")
+    lbl_title.grid(row=2,column=0,pady=15)
+    scl_delay = tk.Scale(frm, from_=0.01, to=0.25, resolution=0.01, orient=tk.HORIZONTAL,command=on_delay_change)
+    scl_delay.set(DELAY)
+    scl_delay.place(x=133,y=147)
+    btn_delay_multi = tk.Button(frm, text="x10", command=set_delay_multi,font="sans 14")
+    btn_delay_multi.place(x=235,y=155)
     btn_exit = tk.Button(frm, text="Exit", command=act_exit,font="sans 14 bold")
-    btn_exit.grid(row=2,column=1,pady=6)
+    btn_exit.grid(row=3,column=1,pady=6)
     lbl_status = ttk.Label(frm,text="Press \"Start\" if you're at the ▶ button.",font="Helvetica 14 italic",justify="left",background="lightgray")
-    lbl_status.grid(row=3,column=0,columnspan=2,sticky = tk.W+tk.E,pady=4)
+    lbl_status.grid(row=4,column=0,columnspan=2,sticky = tk.W+tk.E,pady=4)
     
     root.mainloop()
     
